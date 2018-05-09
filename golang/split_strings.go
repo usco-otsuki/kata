261f967ab96ac6652e964126057a5fddd6057b3c
Otsuki Hitoshi <hitochan777@gmail.com>
2018-05-09 22:12:41 +0900

    golang SplitStrings

---
+++ golang/split_strings.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func SplitStrings(str string) []string {
+	array := []string{}
+	tmp := ""
+	for i := 0; i < len(str); i++ {
+		if len(tmp) <= 1 {
+			tmp += string(str[i])
+		}
+		if len(tmp) == 2 {
+			array = append(array, tmp)
+			tmp = ""
+		}
+	}
+	if len(tmp) == 1 {
+		array = append(array, tmp+"_")
+	}
+
+	return array
+}
+
+func main() {
+	fmt.Println(SplitStrings("abfafda"))
+}

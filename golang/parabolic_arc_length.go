eabc90604cf4a3edd615a5fece0859463471d820
Otsuki Hitoshi <hitochan777@gmail.com>
2018-05-10 22:43:57 +0900

    Golang: Parabolic Arc Length

---
+++ golang/parabolic_arc_length.go
@@ -0,0 +1,21 @@
+package kata
+
+import (
+	"math"
+)
+
+func LenCurve(n int) float64 {
+	len := 0.0
+	width := 1.0 / float64(n)
+	for i := 0; i < n; i++ {
+		x1 := float64(i) * width
+		x2 := float64(i+1) * width
+		y1 := x1 * x1
+		y2 := x2 * x2
+		dx := x2 - x1
+		dy := y2 - y1
+		len += math.Sqrt(dx*dx + dy*dy)
+	}
+
+	return len
+}
---
+++ golang/parabolic_arc_length_test.go
@@ -0,0 +1,36 @@
+package kata
+
+import (
+	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"math"
+)
+
+func assertFuzzyEquals(act float64, exp float64) {
+	var inrange bool
+	var merr float64 = 1e-6
+	var e float64
+	if exp == 0.0 {
+		e = math.Abs(act)
+	} else {
+		e = math.Abs((act - exp) / exp)
+	}
+	inrange = (e <= merr)
+	if inrange == false {
+		fmt.Printf("Expected should be near: %1.6e , but got: %1.6e\n", exp, act)
+	}
+	Expect(inrange).To(Equal(true))
+}
+func dotest(n int, exp float64) {
+	assertFuzzyEquals(LenCurve(n), exp)
+}
+
+var _ = Describe("Test Example", func() {
+	It("should handle basic cases", func() {
+		dotest(1, 1.414213562)
+		dotest(10, 1.478197397)
+		dotest(40, 1.478896272)
+		dotest(200, 1.478940994)
+	})
+})

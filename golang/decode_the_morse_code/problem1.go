b66c8042afd1225836c49c7f8a98984ae3bc6919
Otsuki Hitoshi <hitochan777@gmail.com>
2018-05-14 07:54:45 +0900

    Golang: Decode the Morse code part1

---
+++ golang/decode_the_morse_code/problem1.go
@@ -0,0 +1,19 @@
+package decode_the_morse_code
+
+import (
+	"strings"
+)
+
+func DecodeMorse(morseCode string) string {
+	words := strings.Split(strings.TrimSpace(morseCode), "   ")
+	words_decoded := []string{}
+	for _, word := range words {
+		chars := strings.Split(word, " ")
+		chars_decoded := []string{}
+		for _, char := range chars {
+			chars_decoded = append(chars_decoded, MORSE_CODE[char])
+		}
+		words_decoded = append(words_decoded, strings.Join(chars_decoded, ""))
+	}
+	return strings.Join(words_decoded, " ")
+}

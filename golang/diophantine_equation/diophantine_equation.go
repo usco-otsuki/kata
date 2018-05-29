26eb8a0b46699c931eddbcf58b708cc4c177ca3d
Otsuki Hitoshi <hitochan777@gmail.com>
2018-05-29 20:11:46 +0900

    Golang: Sort slice of slice of int

--- golang/diophantine_equation/diophantine_equation.go
+++ golang/diophantine_equation/diophantine_equation.go
@@ -1,7 +1,23 @@
 package diophantine_equation
 
+import "sort"
+
+type Pairs [][]int
+
+func (p Pairs) Len() int {
+	return len(p)
+}
+
+func (p Pairs) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+func (p Pairs) Less(i, j int) bool {
+	return p[i][0] > p[j][0]
+}
+
 func Solequa(n int) [][]int {
-	result := [][]int{}
+	result := Pairs{}
 	for i := 1; i*i <= n; i++ {
 		if n%i == 0 {
 			a := i
@@ -17,5 +33,6 @@ func Solequa(n int) [][]int {
 			result = append(result, []int{x, y})
 		}
 	}
+	sort.Sort(result)
 	return result
 }

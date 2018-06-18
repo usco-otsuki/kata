2386cc4b11a8af450a3f3decd05c388cdbd290f8
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-19 08:18:04 +0900

    Add hint

--- golang/best_travel/main.go
+++ golang/best_travel/main.go
@@ -1,5 +1,10 @@
 package best_travel
 
 func ChooseBestSum(t, k int, ls []int) int {
-	// your code
+	/*
+	    V[i, w]
+		  = 0            if i=0 or w=0
+	      = V(i-w, w)    if wi> w
+		  = V(i-w, w-wi) otherwise
+	*/
 }

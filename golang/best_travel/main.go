ab3643eb436b671ada1253def147255072b7bf65
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-19 20:57:48 +0900

    Golang: Best Travel

--- golang/best_travel/main.go
+++ golang/best_travel/main.go
@@ -1,10 +1,27 @@
 package best_travel
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func chooseBestSum(t, k, s int, ls []int) int {
+	if t < 0 {
+		return -1
+	}
+	if k == 0 {
+		return s
+	}
+	if len(ls) == 0 {
+		return -1
+	}
+
+	m := max(chooseBestSum(t-ls[0], k-1, s+ls[0], ls[1:]), chooseBestSum(t, k, s, ls[1:]))
+	return m
+}
+
 func ChooseBestSum(t, k int, ls []int) int {
-	/*
-	    V[i, w]
-		  = 0            if i=0 or w=0
-	      = V(i-w, w)    if wi> w
-		  = V(i-w, w-wi) otherwise
-	*/
+	return chooseBestSum(t, k, 0, ls)
 }

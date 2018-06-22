695a23e3805f3b0bb9111f4b910ab506452e2f07
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-22 23:33:27 +0900

    Golang: Longest Common Subsequence

--- golang/lcs/main.go
+++ golang/lcs/main.go
@@ -1,18 +1,28 @@
 package lcs
 
+func max(strings ...string) string {
+	maxString := ""
+	for _, str := range strings {
+		if len(str) > len(maxString) {
+			maxString = str
+		}
+	}
+	return maxString
+}
+
 // LCS returns the longest subsequence common to s1 and s2
 func LCS(s1, s2 string) string {
-	p1, p2 := 0, 0
-	i, j := 0, 0
-	s := ""
-	for p1 < len(s1) && p2 < len(s2) {
-		for i := p1; i < len(s1); i++ {
-			for j := p2; j < len(s2); j++ {
-				if s1[i] == s2[j] {
-					s += string(s1[i])
-				}
+	dp := make([][]string, 2)
+	dp[0] = make([]string, len(s2)+1)
+	dp[1] = make([]string, len(s2)+1)
+
+	for i := 1; i <= len(s1); i++ {
+		for j := 1; j <= len(s2); j++ {
+			dp[i%2][j] = max(dp[(i-1)%2][j], dp[i%2][j-1])
+			if s1[i-1] == s2[j-1] {
+				dp[i%2][j] = max(dp[i%2][j], dp[(i-1)%2][j-1]+string(s1[i-1]))
 			}
 		}
 	}
-	return ""
+	return dp[len(s1)%2][len(s2)]
 }

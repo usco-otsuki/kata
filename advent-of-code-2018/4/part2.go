1451f6a2fce46431e1af2ac1b0a16a3ff863b141
Otsuki Hitoshi <hitochan777@gmail.com>
2018-12-11 22:47:28 +0900

    AoC Day4 Part2

---
+++ advent-of-code-2018/4/part2.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"sort"
+)
+
+type duration struct {
+	start, end int
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	lines := []string{}
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+	sort.Strings(lines)
+	lines = append(lines, "[0000-00-00 00:00] Guard #0 begins shift")
+
+	re := regexp.MustCompile(`\[.+:(\d+)\] (.+)`)
+	guard := 0
+	sleptAt := 0
+	counter := make(map[int]map[int]int)
+	sum := make(map[int]int)
+	for _, line := range lines {
+		matches := re.FindStringSubmatch(line)
+		if matches == nil {
+			log.Fatalf("not match")
+		}
+		var min int
+		fmt.Sscanf(matches[1], "%d", &min)
+		msg := matches[2]
+		switch msg {
+		case "falls asleep":
+			sleptAt = min
+		case "wakes up":
+			if _, ok := counter[guard]; !ok {
+				counter[guard] = make(map[int]int)
+			}
+			for i := sleptAt; i < min; i++ {
+				counter[guard][i]++
+			}
+			sum[guard] += min - sleptAt
+		default:
+			if n, err := fmt.Sscanf(msg, "Guard #%d begins shift", &guard); n == 0 || err != nil {
+				log.Fatalf("Could not parse 'guard begins shift'")
+			}
+		}
+	}
+	maxGuard := 0
+	maxCount := 0
+	maxMinute := 0
+	for guard, minutes := range counter {
+		for minute, c := range minutes {
+			if maxCount < c {
+				maxCount = c
+				maxGuard = guard
+				maxMinute = minute
+			}
+		}
+	}
+	fmt.Println(maxGuard, maxMinute)
+	fmt.Println(maxGuard * maxMinute)
+}

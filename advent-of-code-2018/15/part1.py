ea87f49e1043ea9a0936142deb39bb7e6f2fc79e
Otsuki Hitoshi <hitochan777@gmail.com>
2019-01-08 01:16:08 +0900

    WIP: AoC Day15 Part1

--- advent-of-code-2018/15/part1.py
+++ advent-of-code-2018/15/part1.py
@@ -1,7 +1,9 @@
 import fileinput
+from typing import Optional
 
 class Unit:
-    def __init__(self, x, y, kind, ap = 3, hp = 200, field = None):
+    reading_order = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+    def __init__(self, x, y, kind, field: "Field", ap = 3, hp = 200):
         self.x = x
         self.y = y
         self.kind = kind
@@ -9,48 +11,78 @@ class Unit:
         self.hp = hp
         self.field = field
 
-    def set_field(self, field):
-        pass
-
     def proess_round(self):
-        pass
+        if self.hp <= 0:
+            return
 
-    def move(self):
-        pass
+        direction = self.find_closest_direction()
+        if direction != None:
+            dx, dy = direction
+            self.field[self.x][self.y] = "."
+            self.x += dx
+            self.y += dy
+            self.field[self.x][self.y] = self.kind
+        enemy = self.find_enemy_to_attack()
+        if enemy != None:
+            enemy.reduce_hp(self.ap)
+
+    def get_enemy_kind(self):
+        if self.kind == "G":
+            return "E"
+        else:
+            return "G"
+
+    def find_enemy_to_attack(self) -> Optional["Unit"]:
+        for dx, dy in self.reading_order:
+            if self.field.field[self.x + dx][self.y + dy] == self.get_enemy_kind():
+                return self.field.units[(self.x + dx, self.y + dy)]
+
+        return None
+
+    def find_closest_direction(self):
+        # TODO: to implement
+        return (0,0)
     
-    def attach(self):
-        pass
+    def reduce_hp(self, ap):
+        self.hp = max(self.hp - ap, 0)
+        if self.hp == 0:
+            self.field.field[self.x][self.y] = "."
 
 
 class Field:
     def __init__(self, field):
         self.field = field
-        self.units = [Unit(x,y,cell) for y, cell in enumerate(row) for x, row in enumerate(field) if cell in ["G", "E"]]
+        self.units = {(x, y): Unit(x, y, cell, field=self) for y, cell in enumerate(row) for x, row in enumerate(field) if cell in ["G", "E"]}
     
-    def proces_round(self):
-        self.units = sorted(self.units, lambda unit: (unit.x, unit.y) )
-        for unit in self.units:
+    def process_round(self):
+        unit_list = sorted(self.units.items(), lambda item: (item[1].unit.x, item[1].unit.y) )
+        for x, y, unit in unit_list:
             unit.proces_round()
+            if unit.hp == 0:
+                del self.units[(x, y)]
 
     def is_complete(self):
-        pass
+        kinds = set([unit.kind for unit in self.units])
+        return len(kinds) == 1
 
     def get_sum_hp(self):
         return sum(unit.hp for unit in self.units)
 
 
 if __name__ == "__main__":
-    field = []
+    field_list = []
     for line in fileinput.input():
         row = list(line.strip())
-        field.append(row)
+        field_list.append(row)
+
+    field = Field(field_list)
 
     round = 0
     while True:
-        field.proces_round()
+        field.process_round()
         round += 1 
         if field.is_complete():
             sum_hp = field.get_sum_hp()
             result = sum_hp * round
             print(result)
-            return
+            break

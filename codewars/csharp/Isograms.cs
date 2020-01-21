de840cc158df18eb5bd0b776ffcc63ad805878fb
hitochan777 <hitochan777@gmail.com>
2020-01-21 23:03:23 +0900

    Update Isograms.cs

--- codewars/csharp/Isograms.cs
+++ codewars/csharp/Isograms.cs
@@ -1,17 +1,16 @@
 using System;
 using System.Collections.Generic;
 
-public class Kata
-{
-	public static bool IsIsogram(string str) {
-    str = str.ToLower();
-	  Dictionary<char, bool> exists = new Dictionary<char, bool>();
-	  for (int i = 0; i < str.Length; i++) {
-	  	if (exists.ContainsKey(str[i])) {
-			  return false;
-		  }
-		  exists[str[i]] = true;
-	  }
-	  return true;
-  }
+public class Kata {
+    public static bool IsIsogram(string str) {
+        str = str.ToLower();
+        Dictionary<char, bool> exists = new Dictionary<char, bool>();
+	for (int i = 0; i < str.Length; i++) {
+	    if (exists.ContainsKey(str[i])) {
+	        return false;
+	    }
+	    exists[str[i]] = true;
+	}
+	return true;
+    }
 }

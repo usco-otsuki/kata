e99282d2a49f8908cb955f3fd84d864901b5d267
Otsuki Hitoshi <hitochan777@gmail.com>
2020-03-20 23:58:44 +0900

    Top3

---
+++ codewars/csharp/src/Top3.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class TopWords
+{
+    public static List<string> Top3(string s)
+    {
+        Regex rx = new Regex(@"('[a-zA-Z])+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        MatchCollection matches = rx.Matches(s);
+        var list = new List<string>();
+        foreach (Match match in matches)
+        {
+            list.Add(match.Groups["word"].Value.ToLower());
+        }
+        return list.GroupBy(w => w).OrderByDescending(g => g.Count()).Take(3).Select(g => g.Key).ToList();
+    }
+}

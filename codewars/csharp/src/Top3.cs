5f67177bd22f8566a2869ba3e8d7529e4265722a
Otsuki Hitoshi <hitochan777@gmail.com>
2020-03-21 00:06:28 +0900

    Fix bug in top3

--- codewars/csharp/src/Top3.cs
+++ codewars/csharp/src/Top3.cs
@@ -6,13 +6,21 @@ public class TopWords
 {
     public static List<string> Top3(string s)
     {
-        Regex rx = new Regex(@"('[a-zA-Z])+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        Regex rx = new Regex(@"('*[a-zA-Z]'*)+", RegexOptions.Compiled | RegexOptions.IgnoreCase);
         MatchCollection matches = rx.Matches(s);
         var list = new List<string>();
         foreach (Match match in matches)
         {
-            list.Add(match.Groups["word"].Value.ToLower());
+            list.Add(match.Value.ToLower());
         }
         return list.GroupBy(w => w).OrderByDescending(g => g.Count()).Take(3).Select(g => g.Key).ToList();
     }
 }
+
+    //     return Regex.Matches(s.ToLowerInvariant(), @"('*[a-z]'*)+")
+    //         .GroupBy(match => match.Value)
+    //         .OrderByDescending(g => g.Count())
+    //         .Select(p => p.Key)
+    //         .Take(3)
+    //         .ToList();
+    // }
